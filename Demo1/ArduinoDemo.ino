f6a1dde8a4e25ac600fcd58c01ab2818388db85e
OliveThePuffin <34463898+OliveThePuffin@users.noreply.github.com>
2021-03-12 14:42:54 -0700

    Create ArduinoDemo.ino

---
+++ Demo1/ArduinoDemo.ino
@@ -0,0 +1,258 @@
+#include "DualMC33926MotorShield.h"
+#include <Wire.h>
+#define TICKS_PER_ROT     3200  //50*64
+#define FAIRRER           85    //Fair encoder error
+#define DUTY_CYCLE        127
+//@ no wheel/torque resistance FAIRRER should be a fairly high number, close to 85 for 127 Duty Cycle
+//@ high wheel/torque resistance FAIRRER should be close to 0
+#define WHEEL_RADIUS      0.05  //meters
+#define ROBOT_WIDTH       0.1   //meters
+
+DualMC33926MotorShield md;
+
+//Pin assignments
+const int ENCODER[]     = {6, 5, 13, 11};   //1A,B; 2A,B
+const int ENCODER_ISR[] = {2, 3};           //1,2
+const int MOTORDIR[]    = {7, 8};           //1,2
+const int MOTORPWM[]    = {9, 10};          //1,2
+
+//Global variables
+int desiredRotation1 = 0;
+int desiredRotation2 = 0;
+int rotation1 = 0;
+int rotation2 = 0;
+
+byte receiveData;
+byte sendData;
+
+int encoder1last = 0;
+int encoder1current = 0;
+int encoder2last = 0;
+int encoder2current = 0;
+
+int lastTime1 = 0;
+int currentTime1 = 0;
+int lastTime2 = 0;
+int currentTime2 = 0;
+
+int deltaT1 = 0;
+int deltaT2 = 0;
+
+double v1 = 0;
+double v2 = 0;
+
+double x = 0;
+double y = 0;
+double phi = 0;
+
+void stopIfFault() {
+  if (md.getFault()) {
+    Serial.println("Motor Controller fault");
+    while(md.getFault()) sleep(1000);
+    }}
+
+void setup() {
+  //set up debug console
+  Serial.begin(115200);
+  Serial.println("Debug console:");
+
+  //Join I2C bus as a slave with address 04
+  Wire.begin(0x04);
+  Wire.onReceive(DataReceive);
+  Wire.onRequest(DataRequest);
+
+  //Encoder pins
+  pinMode(ENCODER[0], INPUT);
+  pinMode(ENCODER[1], INPUT);
+  pinMode(ENCODER[2], INPUT);
+  pinMode(ENCODER[3], INPUT);
+  pinMode(ENCODER_ISR[0], INPUT);
+  pinMode(ENCODER_ISR[1], INPUT);
+
+  //Motor controller pins
+  pinMode(MOTORDIR[0], OUTPUT);
+  pinMode(MOTORDIR[1], OUTPUT);
+  pinMode(MOTORPWM[0], OUTPUT);
+  pinMode(MOTORPWM[1], OUTPUT);
+
+  //Interrupts
+  attachInterrupt(digitalPinToInterrupt(ENCODER_ISR[0]), turn, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_ISR[1]), turn, CHANGE);
+
+  //Timer ISR
+  TIMSK2 = (TIMSK2 & B11111110) | 0x01; //enable timer overflow
+  TCCR2B = (TCCR2B & B11111010) | 0x05; //128:1 prescalar
+
+  md.init();
+}
+
+void loop() {
+  
+  move(0.3048); //1 foot (0.3048 meters)
+  rotate(-90); //90 degrees left
+  move(0.3048); //1 foot (0.3048 meters)
+  //motorspin(desiredRotation1);
+
+}
+
+void move(float distance) {
+  if(distance == 0) return;
+  startX = x;
+  startY = y;
+  
+  //forward
+  if(distance > 0) {
+    digitalWrite(MOTORDIR[0], true);
+    digitalWrite(MOTORDIR[1], false);
+    analogWrite(MOTORPWM[0], DUTY_CYCLE);
+    analogWrite(MOTORPWM[1], DUTY_CYCLE);
+  }
+ if(distance < 0) {
+    digitalWrite(MOTORDIR[0], false);
+    digitalWrite(MOTORDIR[1], true);
+    analogWrite(MOTORPWM[0], DUTY_CYCLE);
+    analogWrite(MOTORPWM[1], DUTY_CYCLE);
+  }
+  
+  while(distance > sqrt((startX - x)^2 + (startY - y)^2)) {
+    Serial.println(sqrt((startX - x)^2 + (startY - y)^2));
+  }
+}
+
+void rotate(float degrees) {
+  startPhi = phi;
+  
+  //rotate left
+  if (degrees < 0) {
+    digitalWrite(MOTORDIR[0], false);
+    digitalWrite(MOTORDIR[1], false);
+    analogWrite(MOTORPWM[0], DUTY_CYCLE);
+    analogWrite(MOTORPWM[1], DUTY_CYCLE);
+    while(phi + startPhi > degrees) {
+      sendData = int(rotation1/16);
+      Serial.println(sendData);
+    }
+  }
+  
+  //rotate right
+  else if(degrees > 0) {
+    digitalWrite(MOTORDIR[0], true);
+    digitalWrite(MOTORDIR[1], true);
+    analogWrite(MOTORPWM[0], DUTY_CYCLE);
+    analogWrite(MOTORPWM[1], DUTY_CYCLE);
+    while(phi + startPhi > degrees) {
+      sendData = int(rotation1/16);
+      Serial.println(sendData);
+    }
+  }
+  //stop motors
+  analogWrite(MOTORPWM[0], 0);
+  analogWrite(MOTORPWM[1], 0);
+}
+
+
+ISR(TIMER2_OVF_vect) {
+  TCNT2 = 30; //set timer count to 30 for a ~0.1s period
+  DataRequest(1); //send position data
+}
+
+void DataReceive(int numBytes) {
+  while(Wire.available()) {
+    receiveData = Wire.read();
+  }
+}
+
+//send data through i2c
+void DataRequest(int numBytes) {
+  Wire.write(sendData);
+}
+
+void turn() {
+  //wheel 1
+  encoder1last = encoder1current;
+  encoder1current = digitalRead(ENCODER[0])*2 + digitalRead(ENCODER[1]);
+  if (
+    (encoder1last == 1 && encoder1current == 3) || 
+    (encoder1last == 3 && encoder1current == 2) ||
+    (encoder1last == 2 && encoder1current == 0) ||
+    (encoder1last == 0 && encoder1current == 1)) {
+      updateLocRot(0, 1);
+      
+      }
+  else if (
+    (encoder1last == 3 && encoder1current == 1) || 
+    (encoder1last == 1 && encoder1current == 0) ||
+    (encoder1last == 0 && encoder1current == 2) ||
+    (encoder1last == 2 && encoder1current == 3)) {
+      updateLocRot(0, -1);
+      }
+
+  //wheel 2
+  encoder2last = encoder2current;
+  encoder2current = digitalRead(ENCODER[2])*2 + digitalRead(ENCODER[3]);
+  if (
+    (encoder1last == 1 && encoder1current == 3) || 
+    (encoder1last == 3 && encoder1current == 2) ||
+    (encoder1last == 2 && encoder1current == 0) ||
+    (encoder1last == 0 && encoder1current == 1)) {
+      updateLocRot(1, -1);
+      }
+  else if (
+    (encoder1last == 3 && encoder1current == 1) || 
+    (encoder1last == 1 && encoder1current == 0) ||
+    (encoder1last == 0 && encoder1current == 2) ||
+    (encoder1last == 2 && encoder1current == 3)) {
+      updateLocRot(1, 1);
+      }
+}
+
+updateLocRot(bool wheel, int dir) {
+  //Left
+  if(!wheel) {
+    rotation1 += dir;
+    lastTime1 = currentTime1;
+    currentTime1 = millis();
+    deltaT1 = currentTime1 - lastTime1;
+    v1 = WHEEL_RADIUS*dir*(2*PI/TICKS_PER_ROTATION) / ((float)deltaT1/1000);
+    x = x + cos(phi)*(v1*deltaT1)/2;
+    y = y + sin(phi)*(v1*deltaT1)/2;
+    phi = phi + (WHEEL_RADIUS/ROBOT_WIDTH)*(v1*deltaT1);
+  }
+  
+  //Right
+  else(!wheel) {
+    rotation2 += dir;
+    lastTime2 = currentTime2;
+    currentTime2 = millis();
+    deltaT2 = currentTime2 - lastTime2;
+    v2 = WHEEL_RADIUS*dir*(2*PI/TICKS_PER_ROTATION) / ((float)deltaT2/1000);
+    x = x + cos(phi)*(v2*deltaT2)/2;
+    y = y + sin(phi)*(v2*deltaT2)/2;
+    phi = phi - (WHEEL_RADIUS/ROBOT_WIDTH)*(v2*deltaT2);
+  }
+}
+
+void motorspin(int desiredRotation) {
+  stopIfFault();
+  if(desiredRotation > rotation1) {
+    digitalWrite(MOTORDIR[0], false);
+    analogWrite(MOTORPWM[0], DUTY_CYCLE);
+    while(rotation1 < desiredRotation - FAIRRER) {
+      sendData = int(rotation1 / 16);
+      Serial.println(sendData);
+      stopIfFault();
+    }
+  }
+  else if(desiredRotation < rotation1) {
+    digitalWrite(MOTORDIR[0], true);
+    analogWrite(MOTORPWM[0], DUTY_CYCLE);
+    while(rotation1 > desiredRotation + FAIRRER) {
+      sendData = int(rotation1 / 16);
+      Serial.println(sendData);
+      stopIfFault();
+    }
+  }
+  
+  //Serial.println("Wheel stopped!");
+  analogWrite(MOTORPWM[0], 0);
+}
